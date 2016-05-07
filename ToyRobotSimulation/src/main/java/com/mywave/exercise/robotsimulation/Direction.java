a3e12afdeee61792d0e2eb6333e0b708fb9e477f
Roger Xia <rxia@iselect.com.au>
2016-05-07 14:53:51 +1000

    direction class comments

---
+++ ToyRobotSimulation/src/main/java/com/mywave/exercise/robotsimulation/Direction.java
@@ -0,0 +1,31 @@
+package com.mywave.exercise.robotsimulation;
+
+public enum Direction {
+
+	WEST, NORTH, EAST, SOUTH;
+
+	/* 
+	 * Based on ordinal() property to calculate left
+	 * left direction from current direction
+	 */
+	public Direction left() {
+		int position = ordinal() - 1;
+		Direction[] directions = Direction.values();
+		int max = directions.length - 1;
+		int index = position < 0 ? max : position;
+		return Direction.values()[index];
+	}
+
+	/* 
+	 * Based on ordinal() property to calculate right
+	 * right direction from current direction
+	 */
+	public Direction right() {
+		int position = ordinal() + 1;
+		Direction[] directions = Direction.values();
+		int max = directions.length - 1;
+		int index = position > max ? 0 : position;
+		return Direction.values()[index];
+	}
+
+}
