c1e4002b7b23392b8447f36c07dad82d36310d1e
Roger Xia <rxia@iselect.com.au>
2016-05-07 15:28:44 +1000

    Position Class and Test cases are done

---
+++ ToyRobotSimulation/src/main/java/com/mywave/exercise/robotsimulation/Robot.java
@@ -0,0 +1,74 @@
+package com.mywave.exercise.robotsimulation;
+
+public class Robot {
+
+	public final static int MOVE_FORWARD = 1;
+	public final static int MOVE_BACK = -1;
+	public static final String FORMAT =  "Ouput: %s %s\n";
+			
+	private Position position;
+	private Direction direction;
+ 
+	public Robot() {
+
+	}
+
+	public void left() {
+		direction = direction.left();
+	}
+
+	public void right() {
+		direction = direction.right();
+	}
+
+	public void report() {
+ 		System.out.printf(FORMAT,position,direction.toString());
+	}
+
+	public void place(Position position, Direction direction) {
+		setPosition(position);
+		setDirection(direction);
+	}
+
+	// move based on the direction
+	public void move() {
+		switch (direction) {
+		case WEST:
+			move(MOVE_BACK, 0);
+			break;
+		case NORTH:
+			move(0, MOVE_FORWARD);
+			break;
+		case EAST:
+			move(MOVE_FORWARD, 0);
+			break;
+		case SOUTH:
+			move(0, MOVE_BACK);
+			break;
+		}
+	}
+
+	private void move(int xMove, int yMove) {
+		int xCurrent = position.getX();
+		int yCurrent = position.getY();
+
+		position.setX(xCurrent + xMove);
+		position.setY(yCurrent + yMove);
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+}
---
+++ ToyRobotSimulation/src/test/java/com/mywave/exercise/robotsimulation/RobotTest.java
@@ -0,0 +1,81 @@
+package com.mywave.exercise.robotsimulation;
+ 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RobotTest {
+	
+	private Robot robot;
+	
+	@Before
+	public void setup(){
+		robot = new Robot();
+		Position position = new Position(0, 0);
+		Direction direction = Direction.NORTH;
+		robot.place(position, direction);
+	}
+	
+	@Test
+	public void testPlace(){
+		assertEquals("0,0,", robot.getPosition().toString());
+		assertEquals(Direction.NORTH, robot.getDirection());
+	}
+	
+	@Test
+	public void testMove(){
+ 		robot.move();
+		assertEquals("0,1,", robot.getPosition().toString());
+		assertEquals(Direction.NORTH, robot.getDirection());
+	}
+
+	@Test
+	public void testReport(){
+		robot.move();
+ 		robot.report();
+ 		assertEquals("0,1,", robot.getPosition().toString());
+		assertEquals(Direction.NORTH, robot.getDirection());
+ 	}
+	
+	@Test
+	public void testLeft(){
+		Position position = new Position(1, 2);
+		Direction direction = Direction.EAST;
+		robot.place(position, direction);
+		robot.move();
+		robot.move();
+		robot.left();
+		robot.move();
+		robot.report();
+		assertEquals("3,3,", robot.getPosition().toString());
+		assertEquals(Direction.NORTH, robot.getDirection());
+  	}
+	
+	@Test
+	public void testRight(){
+		Position position = new Position();
+		position.setX(3);
+		position.setY(2);
+		Direction direction = Direction.WEST;
+		robot.place(position, direction);
+		robot.move();
+		robot.move();
+		robot.right();
+		robot.move();
+		robot.report();
+		assertEquals("1,3,", robot.getPosition().toString());
+		assertEquals(Direction.NORTH, robot.getDirection());
+  	}
+	
+	@Test
+	public void testSouth(){
+		Position position = new Position(3, 2);
+		Direction direction = Direction.SOUTH;
+		robot.place(position, direction);
+		robot.move();
+ 		assertEquals("3,1,", robot.getPosition().toString());
+		assertEquals(Direction.SOUTH, robot.getDirection());
+  	}
+	
+}
